47ea6243e32e1b496327a6d789e7321ba69133ab
kimhj <guswlrla1999@naver.com>
2020-05-07 01:47:46 +0900

    kimhyooo_branch_test + activity_version_quiz.xml

---
+++ app/src/main/java/com/example/ansys/hahahaha/activity_version_quiz.java
@@ -0,0 +1,13 @@
+package com.example.ansys.hahahaha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_version_quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_version_quiz);
+    }
+}
