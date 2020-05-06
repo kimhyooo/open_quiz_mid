a6f53a234351400eba2e3f3aacd90e4380664284
kimhj <guswlrla1999@naver.com>
2020-05-07 03:02:43 +0900

    UI수정-> 난이도 모드 합치고 easy에만 힌트버튼 추가, 타이머 등 other.xml없앰

--- app/src/main/java/com/example/ansys/hahahaha/activity_open_quiz.java
+++ app/src/main/java/com/example/ansys/hahahaha/activity_open_quiz.java
@@ -28,14 +28,7 @@ public class activity_open_quiz extends AppCompatActivity {
     private boolean mTimerRunning;
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
-
-
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    int b = activity_other.a;
-
-
-    Button hint1, hint2, hint3, hint4, hint5, hint6, hint7, hint8, hint9, hint10 ;
 
 
     // Question 1
@@ -70,152 +63,6 @@ public class activity_open_quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_quiz);
-
-        //연결
-        hint1 = (Button)findViewById(R.id.hint1);
-        hint2 = (Button)findViewById(R.id.hint2);
-        hint3 = (Button)findViewById(R.id.hint3);
-        hint4 = (Button)findViewById(R.id.hint4);
-        hint5 = (Button)findViewById(R.id.hint5);
-        hint6 = (Button)findViewById(R.id.hint6);
-        hint7 = (Button)findViewById(R.id.hint7);
-        hint8 = (Button)findViewById(R.id.hint8);
-        hint9 = (Button)findViewById(R.id.hint9);
-        hint10 = (Button)findViewById(R.id.hint10);
-
-
-        if(b==1){
-            hint1.setVisibility(View.VISIBLE);
-            hint2.setVisibility(View.VISIBLE);
-            hint3.setVisibility(View.VISIBLE);
-            hint4.setVisibility(View.VISIBLE);
-            hint5.setVisibility(View.VISIBLE);
-            hint6.setVisibility(View.VISIBLE);
-            hint7.setVisibility(View.VISIBLE);
-            hint8.setVisibility(View.VISIBLE);
-            hint9.setVisibility(View.VISIBLE);
-            hint10.setVisibility(View.VISIBLE);
-        }
-        else {
-            hint1.setVisibility(View.INVISIBLE);
-            hint2.setVisibility(View.INVISIBLE);
-            hint3.setVisibility(View.INVISIBLE);
-            hint4.setVisibility(View.INVISIBLE);
-            hint5.setVisibility(View.INVISIBLE);
-            hint6.setVisibility(View.INVISIBLE);
-            hint7.setVisibility(View.INVISIBLE);
-            hint8.setVisibility(View.INVISIBLE);
-            hint9.setVisibility(View.INVISIBLE);
-            hint10.setVisibility(View.INVISIBLE);
-        }
-
-        //이벤트처리
-        hint1.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("1번문제 힌트");
-                dlg.setMessage("DNA");
-                dlg.show();
-            }
-        });
-
-        hint2.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("2번문제 힌트");
-                dlg.setMessage("Vulcanizing");
-                dlg.show();
-            }
-        });
-
-        hint3.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("3번문제 힌트");
-                dlg.setMessage("1, 3");
-                dlg.show();
-            }
-        });
-
-        hint4.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("4번문제 힌트");
-                dlg.setMessage("Gravity");
-                dlg.show();
-            }
-        });
-        hint5.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("5번문제 힌트");
-                dlg.setMessage("Pine trees");
-                dlg.show();
-            }
-        });
-        hint6.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("6번문제 힌트");
-                dlg.setMessage("Clouds OR Cloud");
-                dlg.show();
-            }
-        });
-        hint7.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("7번문제 힌트");
-                dlg.setMessage("3, 4");
-                dlg.show();
-            }
-        });
-
-        hint8.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("8번문제 힌트");
-                dlg.setMessage("Wrist");
-                dlg.show();
-            }
-        });
-
-        hint9.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("9번문제 힌트");
-                dlg.setMessage("Stalagmites");
-                dlg.show();
-            }
-        });
-
-        hint10.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(activity_open_quiz.this);
-                dlg.setTitle("10번문제 힌트");
-                dlg.setMessage("Smelting");
-                dlg.show();
-            }
-        });
 
         //타이머
         mTextViewCountDown = findViewById(R.id.text_view_countdown); //텍스트
@@ -307,7 +154,7 @@ public class activity_open_quiz extends AppCompatActivity {
         int final_score;
 
         //------------------------------------------------------------------------------------------
-        // Question 1 - Correct Answer is #3 (DNA)
+        // Question 1 - Correct Answer is #3 (main method)
         //------------------------------------------------------------------------------------------
         Boolean answer1;
 
@@ -319,18 +166,18 @@ public class activity_open_quiz extends AppCompatActivity {
             answer1_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 2 - Correct Answer is "Vulcanizing"
+        // Question 2 - Correct Answer is "this"
         //------------------------------------------------------------------------------------------
         String answer2;
         question2_answer = (EditText) this.findViewById(R.id.question2_answer);
         answer2 = question2_answer.getText().toString().toLowerCase();
-        if (answer2.equals("vulcanizing")) {
+        if (answer2.equals("this")) {
             answer2_score = 1;
         } else {
             answer2_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 3  - Correct Answers are #1 (Ribosomes) and #3 (Golgi Apparatus)
+        // Question 3  - Correct Answers are 2번 빼고 다 정답
         //------------------------------------------------------------------------------------------
         Boolean answer3_choice1;
         Boolean answer3_choice2;
@@ -344,24 +191,24 @@ public class activity_open_quiz extends AppCompatActivity {
         answer3_choice2 = question2_choice2.isChecked();
         answer3_choice3 = question3_choice3.isChecked();
         answer3_choice4 = question3_choice4.isChecked();
-        if (answer3_choice1 && !answer3_choice2 && answer3_choice3 && !answer3_choice4) {
+        if (answer3_choice1 && !answer3_choice2 && answer3_choice3 && answer3_choice4) {
             answer3_score = 1;
         } else {
             answer3_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 4 - Correct Answer is "Gravity"
+        // Question 4 - Correct Answer is "interface"
         //------------------------------------------------------------------------------------------
         String answer4;
         question4_answer = (EditText) this.findViewById(R.id.question4_answer);
         answer4 = question4_answer.getText().toString().toLowerCase();
-        if (answer4.equals("gravity")) {
+        if (answer4.equals("interface")) {
             answer4_score = 1;
         } else {
             answer4_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 5  - Correct Answers is #2 "Pine trees"
+        // Question 5  - Correct Answers is #2 "public"
         //------------------------------------------------------------------------------------------
         Boolean answer5;
         question5_choice2 = (RadioButton) this.findViewById(R.id.question5_choice2);
@@ -372,18 +219,18 @@ public class activity_open_quiz extends AppCompatActivity {
             answer5_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 6 - Correct Answer is "Clouds" or "Cloud"
+        // Question 6 - Correct Answer is "import pkg.*"
         //------------------------------------------------------------------------------------------
         String answer6;
         question6_answer = (EditText) this.findViewById(R.id.question6_answer);
         answer6 = question6_answer.getText().toString().toLowerCase();
-        if (answer6.equals("clouds") || answer6.equals("cloud")) {
+        if (answer6.equals("import pkg.*")) {
             answer6_score = 1;
         } else {
             answer6_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 7  - Correct Answers are #3 (Earth) and #4 (Pluto)
+        // Question 7  - Correct Answers are #3 (None of the mentioned)
         //------------------------------------------------------------------------------------------
         Boolean answer7_choice1;
         Boolean answer7_choice2;
@@ -397,24 +244,24 @@ public class activity_open_quiz extends AppCompatActivity {
         answer7_choice2 = question7_choice2.isChecked();
         answer7_choice3 = question7_choice3.isChecked();
         answer7_choice4 = question7_choice4.isChecked();
-        if (!answer7_choice1 && !answer7_choice2 && answer7_choice3 && answer7_choice4) {
+        if (!answer7_choice1 && !answer7_choice2 && answer7_choice3 && !answer7_choice4) {
             answer7_score = 1;
         } else {
             answer7_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 8 - Correct Answer is "Wrist"
+        // Question 8 - Correct Answer is "java"
         //------------------------------------------------------------------------------------------
         String answer8;
         question8_answer = (EditText) this.findViewById(R.id.question8_answer);
         answer8 = question8_answer.getText().toString().toLowerCase();
-        if (answer8.equals("wrist")) {
+        if (answer8.equals("java")||answer8.equals(("Java"))) {
             answer8_score = 1;
         } else {
             answer8_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 9  - Correct Answers is #2 "Stalagmites"
+        // Question 9  - Correct Answers is #2 "equals()"
         //------------------------------------------------------------------------------------------
         Boolean answer9;
         question9_choice2 = (RadioButton) this.findViewById(R.id.question9_choice2);
@@ -425,12 +272,12 @@ public class activity_open_quiz extends AppCompatActivity {
             answer9_score = 0;
         }
         //------------------------------------------------------------------------------------------
-        // Question 10 - Correct Answer is "Smelting"
+        // Question 10 - Correct Answer is "int"
         //------------------------------------------------------------------------------------------
         String answer10;
         question10_answer = (EditText) this.findViewById(R.id.question10_answer);
         answer10 = question10_answer.getText().toString().toLowerCase();
-        if (answer10.equals("smelting")) {
+        if (answer10.equals("int")) {
             answer10_score = 1;
         } else {
             answer10_score = 0;
@@ -442,9 +289,9 @@ public class activity_open_quiz extends AppCompatActivity {
                 answer6_score + answer7_score + answer8_score + answer9_score + answer10_score;
 
         if (final_score == 10) {
-            resultsDisplay = "Perfect! You scored 10 out of 10";
+            resultsDisplay = "완벽해!! 10점 만점에 10점~";
         } else {
-            resultsDisplay = "Try again. You scored " + final_score + " out of 10";
+            resultsDisplay = "다시 시도해보렴. 너의 점수는 " + final_score + "/10 이야";
         }
 
         Context context = getApplicationContext();
