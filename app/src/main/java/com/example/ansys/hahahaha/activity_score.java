00fc16be303fbe8cd98901fb261839e319f9829f
kimhj <guswlrla1999@naver.com>
2020-05-24 21:19:20 +0900

    최종 수정 1. 어플 이미지 2. 어플 이름 바꿈 3. score.java에 물리적 뒤로가기 안되도록 설정

--- app/src/main/java/com/example/ansys/hahahaha/activity_score.java
+++ app/src/main/java/com/example/ansys/hahahaha/activity_score.java
@@ -193,6 +193,10 @@ public class activity_score extends AppCompatActivity {
                 startActivity(in);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
 
     }
 }
